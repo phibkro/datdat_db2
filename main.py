a428752f2a0154b69db92f02b76c7e04a17a1c46
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-03-18 11:09:03 +0100

    La til funksjonalitet: dropper tables db2.db hvergang main.py kjøres

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ import buyNineSeats
 if __name__ == "__main__":
   dbPath = "db2.db"
 
+  # Create the database file if it does not exist
+  open(dbPath, 'a').close()
+
+  # Connect to the database
   connection = sqlite3.connect(dbPath)
   
   # Disable auto-commit
   connection.isolation_level = None
   cursor = connection.cursor()
+
+  # Drop all tables
+
+  # Query for all table names
+  cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+  tables = cursor.fetchall()
+  # Drop each table
+  for table in tables:
+      cursor.execute(f"DROP TABLE IF EXISTS {table[0]};")
 
   #------------Oppgave 1------------
   executeSQLFile.executeSQLFromFile("db2_setup.sql", cursor)
