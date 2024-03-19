d5fc8278f878cd4f735cc3f55bd03929d109f1f7
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-03-19 15:28:58 +0100

    Endring fra AnsattNavn til Skuespillernavn på tabell utskrift

--- Oppgave5.sql
+++ Oppgave5.sql
@@ -1,6 +1,6 @@
-SELECT Stykke.Navn AS StykkeNavn, Ansatt.Navn AS AnsattNavn , Rolle.RolleNavn
+SELECT Stykke.Navn AS StykkeNavn, Ansatt.Navn AS SkuespillerNavn , Rolle.RolleNavn
 FROM AktRolle
 INNER JOIN Ansatt ON (AktRolle.AnsattID = Ansatt.ID)
 INNER JOIN Rolle ON (AktRolle.RolleID = Rolle.ID)
 INNER JOIN Stykke ON (AktRolle.StykkeID = Stykke.ID)
-GROUP BY StykkeNavn, AnsattNavn , Rolle.RolleNavn;
+GROUP BY StykkeNavn, SkuespillerNavn , Rolle.RolleNavn;
